fde2d03f2795a54c6ae65a15cd0fe0c1a31bc759
Vladimir <vovasukiasyan@gmail.com>
2019-05-06 21:05:02 +0300

    Merge branch 'dev' of https://github.com/vladimirsukiasyan/TecelaSQL into manukas_mvp
    # Conflicts:
    #	CMakeLists.txt
    #	src/Application/Application.h
    #	src/CMakeLists.txt
    #	src/Command/AddCommand/AddCommand.cpp
    #	src/Command/AddCommand/AddCommand.h
    #	src/Command/Command.h
    #	src/Command/GetCommand/GetCommand.cpp
    #	src/Command/GetCommand/GetCommand.h
    #	src/Command/TouchCommand/TouchCommand.cpp
    #	src/Command/TouchCommand/TouchCommand.h
    #	src/HashTable/HashNode.h
    #	src/HashTable/HashTable.cpp
    #	src/HashTable/HashTable.h
    #	src/Query/Query.cpp
    #	src/Query/Query.h
    #	src/Server/Server.h
    #	src/utils.h
    #	test/main.cpp

--- src/Command/AddCommand/AddCommand.cpp
+++ src/Command/AddCommand/AddCommand.cpp
@@ -1,16 +1,28 @@
 //
-// Created by vladimir on 14.04.19.
+// Created by dmitry on 14.04.19.
 //
 
 #include "AddCommand.h"
 
 void AddCommand::execute() {
+    std::mutex mx_;
+    std::lock_guard<std::mutex> lock(mx_);
+    bool success = pTable->Add(this->key, this->exptime, this->length, this->value);
+    if (success) {
+        // Вызвать setTimer Cachmanager
+        std::string s = "Добавление успешно!";
+        client_socket->send(s);
+    } else
+        throw InvalidTextLineException();
     //выполнение команды
     //тут может быть проверка на время жизни ключа => отправка запроса
     //на CacheManager для установки timer
     //и прочие команды
 }
 
-void AddCommand::setValue(std::byte *value) {
-    this->value=value;
+std::string AddCommand::toStr() {
+    std::string answer =
+            "Add " + this->key + " " + std::to_string(this->exptime) + " " + std::to_string(this->length) + " " +
+            (char *) this->value;
+    return answer;
 }
