4d4d4741c544c05bb0a8d09228ebea6bac745755
Vladimir <vovasukiasyan@gmail.com>
2019-05-12 17:06:37 +0300

    Update Query: realized query parser

--- src/Query/Query.cpp
+++ src/Query/Query.cpp
@@ -3,3 +3,139 @@
 //
 
 #include "Query.h"
+
+Command *Query::getCommand() {
+    return command;
+}
+
+
+//Parse first line(HEAD LINE) from client
+
+void Query::parseHeadLine(const std::string &headBuffer) {
+    //парсинг первой части запроса
+    //<command_string><key><exptime><length>
+    std::vector<std::string> tokens;
+    boost::split(tokens, headBuffer, boost::is_any_of(" "));
+
+    if (tokens.empty()) {
+        throw InvalidHeadLineException();
+    }
+    std::string command_string = tokens[0];
+    boost::to_lower(command_string);
+
+    if (command_string == "get") {
+        method = METHOD::GET;
+
+        if (tokens.size() != GET_COMMAND_HEADLINE_PARAMS_COUNT) {
+            throw InvalidHeadLineException();
+        }
+
+        command = new GetCommand(tokens[1], client_socket);
+    }
+    else if (command_string == "add") {
+        method = METHOD::ADD;
+
+        if (tokens.size() != ADD_COMMAND_HEADLINE_PARAMS_COUNT) {
+            throw InvalidHeadLineException();
+        }
+
+        std::string::size_type sz1 = 0, sz2 = 0;
+        long long exptime;
+        long long length;
+        try {
+            exptime = std::stoll(tokens[2], &sz1);
+            length = std::stoll(tokens[3], &sz2);
+        }
+        catch (std::invalid_argument &e) {
+            throw InvalidHeadLineException();
+        }
+        if (sz1 != tokens[2].size() || sz2 != tokens[3].size())
+            throw InvalidHeadLineException();
+
+        command = new AddCommand(tokens[1], exptime, length, client_socket);
+        return;
+    }
+
+    else if (command_string == "set") {
+        method = METHOD::SET;
+
+        if (tokens.size() != SET_COMMAND_HEADLINE_PARAMS_COUNT) {
+            throw InvalidHeadLineException();
+        }
+
+        std::string::size_type sz1 = 0, sz2 = 0;
+        long long exptime;
+        long long length;
+        try {
+            exptime = std::stoll(tokens[2], &sz1);
+            length = std::stoll(tokens[3], &sz2);
+        }
+        catch (std::invalid_argument &e) {
+            throw InvalidHeadLineException();
+        }
+        if (sz1 != tokens[2].size() || sz2 != tokens[3].size())
+            throw InvalidHeadLineException();
+
+        command = new SetCommand(tokens[1], exptime, length, client_socket);
+        return;
+    }
+
+    else if (command_string == "delete") {
+
+        method = METHOD::DELETE;
+        if(tokens.size()!=DELETE_COMMAND_HEADLINE_PARAMS_COUNT){
+            throw InvalidHeadLineException();
+        }
+
+        command=new DeleteCommand(tokens[1],client_socket);
+
+    }
+
+    else if (command_string == "touch") {
+        method = METHOD::TOUCH;
+    }
+
+    else if (command_string == "incr") {
+        method = METHOD::INCR;
+    }
+
+    else if (command_string == "decr") {
+        method = METHOD::DECR;
+    }
+
+    else if (command_string == "flush_all") {
+        method = METHOD::FLUSH_ALL;
+    }
+
+    else if (command_string == "disconnect") {
+        method = METHOD::DISCONNECT;
+
+        command=new DisconnetCommand(client_socket);
+    }
+
+    else throw InvalidHeadLineException();
+
+}
+
+// If client sent ADD or SET command, server has to receive one more line
+bool Query::hasToSendValueLine() {
+    return method == METHOD::ADD || method == METHOD::SET;
+}
+
+void Query::setCommandValue(const std::string &valueBuffer) {
+    if (method == METHOD::ADD) {
+        ((AddCommand *) command)->setValue((std::byte *) valueBuffer.c_str());
+    }
+    if (method == METHOD::SET) {
+        ((SetCommand *) command)->setValue((std::byte *) valueBuffer.c_str());
+    }
+}
+
+Query::Query(const Socket::ptr &socket) :
+        client_socket(Socket::ptr(socket)){
+}
+
+Query::~Query() {
+    delete command;
+}
+
--- src/Query/Query.h
+++ src/Query/Query.h
@@ -5,13 +5,43 @@
 #ifndef TECELASQL_QUERY_H
 #define TECELASQL_QUERY_H
 
+#include <boost/algorithm/string.hpp>
+#include <iostream>
+
+#include "../Command/Command.h"
+#include "../Command/GetCommand/GetCommand.h"
+#include "../Command/AddCommand/AddCommand.h"
+#include "../Command/SetCommand/SetCommand.h"
+#include "../Command/DeleteCommand/DeleteCommand.h"
+#include "../Command/TouchCommand/TouchCommand.h"
+#include "../Command/IncrCommand/IncrCommand.h"
+#include "../Command/DecrCommand/DecrCommand.h"
+#include "../Command/FlushAllCommand/FlushAllCommand.h"
+#include "../Command/DisconnectCommand/DisconnetCommand.h"
+#include "../utils.h"
+
+const int ADD_COMMAND_HEADLINE_PARAMS_COUNT = 4;
+const int SET_COMMAND_HEADLINE_PARAMS_COUNT = 4;
+const int GET_COMMAND_HEADLINE_PARAMS_COUNT = 2;
+const int DELETE_COMMAND_HEADLINE_PARAMS_COUNT = 2;
 
 class Query {
+    METHOD method;
+    Command *command = nullptr;
+    Socket::ptr client_socket;
+
 public:
-    Command* getCommand();
-private:
-    void parseRequest(std::string request);
-    //прочие функции для реализации getCommand()
+    explicit Query(const Socket::ptr &socket);
+
+    ~Query();
+
+    Command *getCommand();
+
+    void parseHeadLine(const std::string &headBuffer);
+
+    bool hasToSendValueLine();
+
+    void setCommandValue(const std::string &valueBuffer);
 };
 
 
