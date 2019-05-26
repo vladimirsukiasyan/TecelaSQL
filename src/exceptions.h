b91f7bff64a4f3c21c7bdf3a8ebf9bbca854de02
Vladimir <vovasukiasyan@gmail.com>
2019-05-26 23:59:44 +0300

    Added some custom exceptions and modified utils

--- src/exceptions.h
+++ src/exceptions.h
@@ -1,5 +1,3 @@
-#include <utility>
-
 //
 // Created by vladimir on 09.05.19.
 //
@@ -23,5 +21,37 @@ public:
         return msg.c_str();
     }
 };
+
+class NotFoundKeyException : public std::exception {
+
+    std::string msg;
+
+public:
+
+    explicit NotFoundKeyException(std::string msg="This key doesn't exist!"):
+            msg(std::move(msg))
+    {}
+
+    const char *what() const throw() override {
+        return msg.c_str();
+    }
+};
+
+class NotEnoughMemoryException : public std::exception {
+
+    std::string msg;
+
+public:
+
+    explicit NotEnoughMemoryException (std::string msg="Not enough memory!"):
+            msg(std::move(msg))
+    {}
+
+    const char *what() const throw() override {
+        return msg.c_str();
+    }
+};
+
+
 
 #endif //TECELASQL_EXCEPTIONS_H
